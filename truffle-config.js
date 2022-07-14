e00415ff8c798093be81113b2317f0ab1e4df75c
Heesoo Hwang <heesoohwang1231@gmail.com>
2022-07-13 18:35:55 -0700

    Updated the overall render of the project

--- truffle-config.js
+++ truffle-config.js
@@ -1,10 +1,10 @@
 const path = require("path");
 
-const HDWalletProvider = require('truffle-hdwallet-provider');
-const infuraKey = "45235b9ed68b456f81374e2e74bdf94a";
+// const HDWalletProvider = require('truffle-hdwallet-provider');
+// const infuraKey = "45235b9ed68b456f81374e2e74bdf94a";
 
-const fs = require('fs');
-const mnemonic = "often easily cram buffalo apple creek kiwi list aim pretty fade couple"; //fs.readFileSync(".secret").toString().trim();
+// const fs = require('fs');
+// const mnemonic = "often easily cram buffalo apple creek kiwi list aim pretty fade couple"; //fs.readFileSync(".secret").toString().trim();
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
